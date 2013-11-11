aad68fc8901a91f5b8fc0a7927732ec80c196322
justlaputa <justlaputa@gmail.com>
2013-11-11 21:57:12 +0900

    fix xhr problem in firefox os

--- app/js/jenkins.js
+++ app/js/jenkins.js
@@ -15,22 +15,39 @@
     //callback(err, data)
     Jenkins.prototype.getJobs = function(callback) {
 
-        $.ajax({url: this.url + query.jobs, dataType: 'json'}).done(function(data) {
-
-            console.log('get jenkins data: ', data);
-
-            if (callback) {
-                callback(null, data);
-            }
-
-        }).fail(function() {
-
-            console.log('request for jobs fails');
-
-            if (callback) {
-                callback('error', null);
-            }
+        var xhr = new window.XMLHttpRequest({
+            mozSystem: true
         });
+
+        xhr.open('GET', this.url + query.jobs, true);
+        xhr.responseType = 'json';
+
+        xhr.onload = function(e) {
+            var err = null;
+
+            if (xhr.status === 200 || xhr.status === 400 || xhr.status === 0) {
+                console.log('xhr success');
+            } else {
+                err = 'error';
+                console.log('xhr failed: ', xhr.status);
+            }
+
+            if (callback && typeof callback === 'function') {
+                setTimeout(function() {
+                    callback(err, xhr.response);
+                }, 0);
+            }
+        };
+
+        xhr.ontimeout = function(e) {
+            console.log('xhr timeout to get jobs');
+        };
+
+        xhr.onerror = function(e) {
+            console.log('xhr error: ', e);
+        };
+
+        xhr.send();
     };
 
     window.Jenkins = Jenkins;
